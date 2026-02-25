49274b8d0922d563bbd584c8b71727d1e38b8aed
FluctMember <fluct_g_developer@cartahd.com>
2026-02-25 06:30:53 +0000

    add SDK Ver 6.41.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "FluctSDK",
-                      url: "https://github.com/voyagegroup/FluctSDK-iOS-Swift-Package/releases/download/6.40.4/FluctSDK.xcframework.zip", checksum: "539cf3a689be920f5a87e6e99570ff4de73af911468e02677904e774078982a7"),
+                      url: "https://github.com/voyagegroup/FluctSDK-iOS-Swift-Package/releases/download/6.41.0/FluctSDK.xcframework.zip", checksum: "60881ed4e53a278e51a9195d5736992fa67948ac473be89bd05e8667db2c4023"),
         .target(
             name: "FluctSDKResources",
             dependencies: [
